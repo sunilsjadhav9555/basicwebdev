5894eb84d726eed8cbb13331068de4224590d338
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-11-16 15:44:51 +0530

    Add files via upload

---
+++ appes.js
@@ -0,0 +1,11 @@
+
+/*import multiply from "./explore.js";
+import { add, subtract } from "./explore.js"
+
+console.log(multiply(6, 2));
+console.log(add(2, 2));
+*/
+const mathOperations = require("./explore.js");
+
+console.log(mathOperations.multiply(5, 2));
+
