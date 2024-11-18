4842564c9979625d326261787f436807619b5227
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-11-18 18:40:35 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,121 @@
+/* let score = 10 + 11;
+let num1 = 10;
+
+num1 /= 5;
+ 
+console.log(num1);
+console.log(score);
+
+let gameName = "spiderman";
+gameName = "batman";
+console.log(gameName);
+const username = "sunil.com";
+username = "hitesh";
+console.log(username);
+
+
+console.log("hello");
+console.log("Coffee");
+
+var score = 103;
+let score =102;
+let name ="sunil.com";
+let isLoggedin = false;
+console.log(name);
+console.log(isLoggedin);
+console.log(score);
+
+
+let CofeeTypes = [ "black Coffee","Cold Coffee","Americano","late"];
+console.log(CofeeTypes);
+
+let user = {
+    Firstname:"sunil",
+    Lastname:"Jadhav",
+    Developer:"FrontEnd",
+    Framework:"Ract"
+
+};
+
+console.log(user);
+
+
+let getScore = score;
+console.log(getScore);
+
+
+let isLoggedin = true;
+let ispaid = true;
+
+console.log(isLoggedin && ispaid);
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
+
+
+
+let score = ((2 * (3 + 2)) -1);
+console.log(score);
+
+
+
+let score = 102;
+let bonus = 25;
+ 
+let totalscore = score + bonus;
+console.log(totalscore);
+
+let addition = 4 + 5;
+console.log(addition);
+
+let subtract = 9 - 3;
+console.log(subtract);
+
+let mult = 3 * 5;
+console.log(mult);
+
+let div = 8 / 2;
+console.log(div);
+
+let reminder = 9 % 2;
+console.log(reminder);
+
+let expo = 2 ** 3;
+console.log(expo);
+
+
+let myscore = 110;
+myscore++;
+console.log(myscore);
+
+let credits = 56;
+credits--;
+console.log(credits);
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
+
+*/
+
+console.log("hello");
+console.log("Coffee");
+
+
+console.table({city:"pune"});
+console.warn({city:"pune"});
+
+
+
+
+
+
+
+
