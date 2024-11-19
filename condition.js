178055ee705d9f30dfab3849683453e666958dee
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-11-19 09:59:45 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,66 @@
+ /* Checking if a number is greater than another number:
+
+let num1 = 8;
+let num2 = 5;
+
+console.log("regular upper code");
+if (num1 > num2) {
+    console.log("num1 is greater than num 2"); 
+}
+else{
+    console.log("nope, num1 is not greater");
+    
+}
+
+console.log("bottom code");
+
+Checking if a string is equal to another string:
+
+
+
+let username = "chai";
+let anotherusername = "Coffee";
+ if(username == anotherusername){
+    console.log("pick another username");
+ }
+else
+{
+    console.log(`you can pick ${anotherusername} username`);
+    
+}
+
+Checking if a variable is a number or not:
+
+let score = "44";
+if (typeof score === "number"){
+    console.log("yep, this is number"); 
+}
+else{
+    console.log("no that is not a number");
+    
+}
+
+Checking if a boolean value is true or false
+
+
+let iscoffeeready = true;
+if (iscoffeeready){
+    console.log("coffee is ready"); 
+}
+else{
+    console.log("coffee is not ready");
+    
+}
+    */
+   // array is empty or not.
+   let items = ["sj"];
+   console.log(items.length);
+
+   if(items.length === 0){
+    console.log("Array is empty");    
+   }
+   else{
+    console.log("Array is not empty");
+    
+   }
+   
