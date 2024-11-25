2f1f644e72d6d2098dd2bc5f2abf119221111d6d
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-11-25 17:44:15 +0530

    Add files via upload

---
+++ prinonpri.js
@@ -0,0 +1,62 @@
+/*
+let balance = 120;
+let anotherBalance = new Number (120);
+
+console.log(balance);
+console.log(anotherBalance.valueOf());
+
+console.log(typeof balance);
+console.log(typeof anotherBalance);
+
+
+let isActive = true;
+let isReallyActive = new Boolean(true);
+console.log(isActive);
+console.log(isReallyActive);
+
+
+let firstname = null;
+let lastname = undefined;
+console.log(firstname);
+console.log(lastname);
+
+
+let myString = "hello";
+let myStringOne = "hola";
+let username = "Sunil";
+
+let oldGreet = myString + " " + "Sunil " +  myStringOne;
+console.log(oldGreet);
+
+let GreetMessage = `hello ${username} !`;
+console.log(GreetMessage);
+
+let demoOne = `value is ${2 * 2}`;
+console.log(demoOne);
+
+let sm1 = Symbol("sunil");
+let sm2 = Symbol("sunil");
+console.log(sm1);
+console.log(typeof sm1);
+
+
+const username = {
+    firstname:"sunil",
+    isloggedin:true,
+};
+username.firstname = "Mr.H";
+username.Lastname = "Jadhav";
+console.log(username["firstname"]);
+console.log(username.Lastname);
+console.log(username);
+console.log(typeof username);
+
+let today = new Date();
+console.log(today.getDate());
+*/
+let anotherUser = ["hitesh", true];
+console.log(anotherUser[0]);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
