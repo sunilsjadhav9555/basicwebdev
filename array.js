f98e9b4c9500c0318b91286c1305421043e1ea12
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-11-28 20:04:59 +0530

    Add files via upload

--- array.js
+++ array.js
@@ -1,72 +1,53 @@
-/* 
-1. Declare an array named `teaFlavors` that contains the strings `"green tea"`, `"black tea"`, and `"oolong tea"`. 
-   Access the first element of the array and store it in a variable named `firstTea`.
-
-
-let teaflavors =["green tea","black tae","oolong tea"];
-console.log(teaflavors);
-
-let firstTea = teaflavors[0];
-console.log(firstTea);
-
-
-let cities =["london","tokyo","paris","newyork"]
-
-let favoritecity = cities[2];
-console.log(favoritecity);
-
-
-let teatypes = ["herbal tea","white tea","masala chai"];
-teatypes[1] = "jasmine tea"
-console.log(teatypes);
+/*const myArr = [1,2,3,4,5]
+console.log(myArr[4]);
+const myheros = ["baba","aaie"]
+console.log(myheros[1]);
+const myArr1 =new Array(9,8,7,6,5)
+console.log(myArr1[0]);
+myArr.push(6)
+console.log(myArr);
+myArr.push(7)
+console.log(myArr);
+myArr.pop()
+console.log(myArr);
+myArr.unshift(0)
+console.log(myArr);
+myArr.shift()
 
 
 
-let citiesvisited = [ "mumbai","sydney"];
-console.log(citiesvisited);
-citiesvisited.push("berlin");
-console.log(citiesvisited);
+console.log(myArr);
+console.log(myArr.includes(4));
+
+const myArr = [1,2,3,4,5]
+const myArr1 =new Array(9,8,7,6,5)
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3);
+console.log("slice" ,myn1);
+console.log("B",myArr1);
+const myn2 = myArr1.splice(1,3);
+console.log("splice " ,myn2);
+console.log(myArr);
+console.log(myArr1);
 
 
+const marvel_hero =["thor","ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+//marvel_hero.push(dc_heros)
+//console.log(marvel_hero[3][1])
+ const marvel_hero1 = marvel_hero.concat(dc_heros)
+console.log(marvel_hero1);
+const nmarvel = [...marvel_hero,...dc_heros]
+console.log(nmarvel);
+const another = [1,2,3, [4,5,6],7, [8,9],[4,5]]
+const realanother = another.flat(Infinity)
+console.log(realanother);*/
+console.log(Array.isArray("sunil"));
+console.log(Array.from("sunil"));
+console.log(Array.from({name:"sunil"}));// not create intertresting
 
-let teaorders = ["chai","iced tea","matcha","earl-grey"];
-console.log(teaorders);
-let lastorder= teaorders.pop();
-console.log(lastorder);
-console.log(teaorders);
+let score1 = 100
+let score2 = 200
+let score3 = 300
 
-
-
-let popularteas = ["green tea","oolona tea","chai"];
-let softcopyteas = popularteas;
-popularteas.pop();
-
-console.log(popularteas);
-console.log(softcopyteas);
-
-
-
-let topcities = ["berlin","singapore","new York"];
-let hardcopycities = [...topcities];
-let hardcopycities = topcities.slice();
-topcities.pop();
-console.log(hardcopycities);
-
-
-
-let europeancities = ["paris","Rome"];
-let asiancities = ["tokyo","bangkok"];
-let worldcities = europeancities.concat(asiancities);
-console.log(worldcities);
-
-
-
-let teamenu = ["masalachai","oolangtea","greentea","earlgrey"];
-let menulenght = teamenu.length;
-console.log(menulenght);
-
-*/
-
-let citibucketlist = ["kyoto","london","capetown","vancouver"];
-let islondoninlist = citibucketlist.includes("london");
-console.log(citibucketlist);
+console.log(Array.of(score1,score2,score3));
