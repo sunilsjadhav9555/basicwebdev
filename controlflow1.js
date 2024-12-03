6083eac547880b7ec04ba3d5f5f3115852b1eb14
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-03 11:28:07 +0530

    Add files via upload

---
+++ controlflow1.js
@@ -0,0 +1,14 @@
+/*falsy values 
+false ,0 ,-0 ,BigInt 0n,"",null,undefine,nan
+
+truthy value
+"0" ,'false'," ",[],{},function(){}
+
+nullish coalescing opertor (??): null undefin
+val1 = undefined ?? 15 =15
+val1 = null ?? 10 =10
+get the next value of null and undefined value 
+
+ternary opertor
+condition ? true:false
+*/
