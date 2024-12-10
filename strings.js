31dbd9471158d8e3eff0a31ab7d26443670f0c6c
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-10 18:30:42 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,22 @@
+const name = "sunil"
+const repoCount =50
+console.log(name + repoCount + " values");
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String(`sunils`)
+console.log(gameName[1]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('l'));
+ const newstring = gameName.substring(0,4)
+ console.log(newstring);
+ const anotherstring = gameName.slice(-5,3)
+ console.log(anotherstring);
+ const newstringone = " skkk  " 
+ console.log(newstringone);
+ console.log(newstringone.trim());
+ const url = "https://hitesh.com"
+ console.log(url.replace('hitesh','sunil'));
+ console.log(gameName.split('.'));
