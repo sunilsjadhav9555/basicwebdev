c9af8dcfc9457e8cf6af8e4a48139515be39790b
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-11 00:53:50 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,14 @@
+const accountid = 1
+let accountemail = "sunil955@gmail.com"
+var accountpassword ="12345"
+accountcity = "pune"
+accountstate = "mh";
+const accountid2 = 2
+let accountemail1 = "sunil55@gmail.com"
+var accountpassword1 ="1234"
+accountcity1 = "nagar"
+accountstate1 = "mh";
+console.log(accountid);
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate])
+console.log(accountid2);
+console.table([accountid2,accountemail1,accountpassword1,accountcity1,accountstate1])
