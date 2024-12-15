7b697989d5aa3694b05394e4a2e33d2c42a6b20f
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-15 21:44:12 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,29 @@
+if(true){
+
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+function one (){
+    const username = "sunil"
+    function two (){
+        const website ="youtube"
+        console.log(username);
+        console.log(website);
+    }
+  
+    two()
+}
+one()
+
+function addone(num)
+{
+    return num + 1
+}
+addone(5)
