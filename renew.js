87aa6b4bc9a0874d5cfde44824bd83556065ce9e
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-16 11:37:13 +0530

    Add files via upload

---
+++ renew.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "sunil@gmail.com"
+var accountpassword = "12345"
+ let accountcity = "jaipur"
+
+ console.table([accountId,accountEmail,accountpassword,accountCity]);
+ 
+ 
