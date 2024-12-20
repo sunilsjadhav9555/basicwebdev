03ca8f65977a85e1c68edd0d05b42b1fd696d184
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-20 21:04:03 +0530

    Add files via upload

---
+++ opertions.js
@@ -0,0 +1,21 @@
+//let value = 3
+//let negvalue = -value
+//console.log(negvalue);
+//console.log( 2 % 2 );
+//let str1 = "hi"
+//let str2 = " sj"
+//let str3 = str1 + str2
+//console.log(str3);
+//console.log(true);
+//console.log(+true);
+//console.log("");
+//let num1, num2, num3
+/*num1 = num2 = num3 = 2  + 2
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);*/
+let x = 3;
+const y = ++x;
+console.log("x =",x);
+console.log("y =",y);
+
