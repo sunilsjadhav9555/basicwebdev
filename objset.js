43299f9832379805655550a53b4060da9fcecabe
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2024-12-23 11:59:56 +0530

    Add files via upload

---
+++ objset.js
@@ -0,0 +1,21 @@
+const promiseOne = new Promise(function resolve, reject) 
+{
+    // do and async task
+    //db calls crypto network
+    set Timeout(function ()
+{
+    console.log('async task is completed');
+},1000)
+})
+const promiseTwo = new Promise (function (resolve, reject){
+    setTimeout(function () {
+        
+    }, timeout);
+})
+
+const data = response.json()
+console.log(data);
+{
+    console.log(data);
+    
+}
